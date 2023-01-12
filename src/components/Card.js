55f2aa4b9cac8f3e58f4eca52c76836ae83470d5
sarperbg <sarpergulfirat@gmail.com>
2023-01-12 16:26:11 +0300

    gameboard completed

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,11 @@
-import React from 'react'
-
-const Card = ({item, id, handleClick}) => {
+function Card({item, id, handleClick}){
     const itemClass = item.stat ? " active " + item.stat : ""
-  return (
-    <div className={"card" + itemClass}  onClick={() => handleClick(id)}>
-        <img src={item.img} alt={item.id} />
-    </div>
-  )
+
+    return (
+        <div className={"card" + itemClass} onClick={() => handleClick(id)}>
+            <img src={item.img} alt="" />
+        </div>
+    )
 }
 
 export default Card
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,33 +3,60 @@ import Card from './Card'
 const Cards = () => {
     const [items, setItems] = useState([
         {id: 1 , img: '/img/animal1.png' , stat: ""},
+        {id: 1 , img: '/img/animal1.png' , stat: ""},
+        {id: 2 , img: '/img/animal2.png' , stat: ""},
         {id: 2 , img: '/img/animal2.png' , stat: ""},
         {id: 3 , img: '/img/animal3.png' , stat: ""},
+        {id: 3 , img: '/img/animal3.png' , stat: ""},
+        {id: 4 , img: '/img/animal4.png' , stat: ""},
         {id: 4 , img: '/img/animal4.png' , stat: ""},
         {id: 5 , img: '/img/animal5.png' , stat: ""},
+        {id: 5 , img: '/img/animal5.png' , stat: ""},
+        {id: 6 , img: '/img/animal6.png' , stat: ""},
         {id: 6 , img: '/img/animal6.png' , stat: ""},
         {id: 7 , img: '/img/animal7.png' , stat: ""},
+        {id: 7 , img: '/img/animal7.png' , stat: ""},
         {id: 8 , img: '/img/animal8.png' , stat: ""},
-        {id: 9 , img: '/img/animal9.png' , stat: ""},
-        {id: 10 , img: '/img/animal10.png' , stat: ""},
-        {id: 11 , img: '/img/animal11.png' , stat: ""},
-        {id: 12 , img: '/img/animal12.png' , stat: ""},
-        {id: 13 , img: '/img/animal13.png' , stat: ""},
-        {id: 14 , img: '/img/animal14.png' , stat: ""},
-        {id: 15 , img: '/img/animal15.png' , stat: ""},
-        {id: 16 , img: '/img/animal16.png' , stat: ""},
+        {id: 8 , img: '/img/animal8.png' , stat: ""},
       
-    ])
+    ].sort(() => Math.random() - 0.5))
+
     const [prev, setPrev] = useState(-1)
-    function handleClick (id) {
-        items[id].stat = "active"
+
+    function check(current){
+        if(items[current].id == items[prev].id){
+            items[current].stat = "correct"
+            items[prev].stat = "correct"
+            setItems([...items])
+            setPrev(-1)
+        }else{
+            items[current].stat = "wrong"
+            items[prev].stat = "wrong"
+            setItems([...items])
+            setTimeout(() => {
+                items[current].stat = ""
+                items[prev].stat = ""
+                setItems([...items])
+                setPrev(-1)
+            }, 1000)
+        }
+    }
+
+    function handleClick(id){
+        if(prev === -1){
+            items[id].stat = "active"
+            setItems([...items])
+            setPrev(id)
+        }else{
+            check(id)
+        }
     }
    return (
-    <div className='container'>
-        { items.map((item , index) => (
-            <Card key={index} item={item} id={index} handleClick={handleClick}/>
-        )) }
-    </div>
+    <div className="container">
+            { items.map((item, index) => (
+                <Card key={index} item={item} id={index} handleClick={handleClick} />
+            )) }
+        </div>
   )
 }
 

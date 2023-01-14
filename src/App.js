c6d120e0c1ce07cadff1d65c86d49c1713ccff7b
sarperbg <sarpergulfirat@gmail.com>
2023-01-14 17:08:18 +0300

    last commtit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Routes>
           <Route  path="/"element={<LoginPage/>}/>
           <Route  path="/GameBoardPage" element={<GameBoardPage/>}/>
-          <Route  path="/GameBoardPage" element={<GameBoardPage/>}/>
+          <Route  path="/GameSettingsPage" element={<GameSettingsPage/>}/>
 
 
         </Routes>
